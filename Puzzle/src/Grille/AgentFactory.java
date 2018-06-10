d372afc5e778bb98918c064a195304c2242be67a
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2018-06-10 22:59:05 +0200

    agentFactory doneé

---
+++ Puzzle/src/Grille/AgentFactory.java
@@ -0,0 +1,40 @@
+package Grille;
+
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class AgentFactory {
+    private int idAgent = 1;
+    private int nbColumns;
+    private int nbRows;
+    private HashSet<Position> startPositionsUsed;
+    private HashSet<Position> goalPositionsUsed;
+
+    public AgentFactory(int nbColonne, int nbLigne) {
+        this.nbColumns = nbColonne;
+        this.nbRows = nbLigne;
+        startPositionsUsed = new HashSet<>();
+        goalPositionsUsed = new HashSet<>();
+    }
+
+    public Agent create() {
+        Position startPosition;
+        do {
+            int x = ThreadLocalRandom.current().nextInt(0, nbColumns);
+            int y = ThreadLocalRandom.current().nextInt(0, nbRows);
+            startPosition = new Position(x, y);
+        } while (startPositionsUsed.contains(startPosition));
+        Position goalPosition;
+        do {
+            int x = ThreadLocalRandom.current().nextInt(0, nbColumns);
+            int y = ThreadLocalRandom.current().nextInt(0, nbRows);
+            goalPosition = new Position(x, y);
+        } while (goalPositionsUsed.contains(goalPosition)|| goalPosition.equals(startPosition));
+        Agent.Color color = Agent.Color.RED;
+        Agent agent = new Agent(idAgent, "", new Case(startPosition), new Case(goalPosition), color);
+        startPositionsUsed.add(startPosition);
+        goalPositionsUsed.add(goalPosition);
+        idAgent++;
+        return agent;
+    }
+}
